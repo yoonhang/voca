1b644d9dab8438c354ff684f89e4eb9d3fa41d3a
yoonhang <com717942@gmail.com>
2024-05-30 12:28:44 +0900

    bug mod...

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -4,8 +4,7 @@ export default function useFetch(url: string) {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch(url)
-      .then(res => {
+    fetch(url).then(res => {
         return res.json();
       })
       .then(data => {
